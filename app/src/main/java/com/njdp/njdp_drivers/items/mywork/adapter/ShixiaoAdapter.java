c46bfe827d31c4bc83247034ee7aa49589a5fac0
Rock <2017jikai@gmail.com>
2018-07-26 20:20:43 +0800

    【我的作业-我的地块】 修订了条目过滤空数据显示

--- app/src/main/java/com/njdp/njdp_drivers/items/mywork/adapter/ShixiaoAdapter.java
+++ app/src/main/java/com/njdp/njdp_drivers/items/mywork/adapter/ShixiaoAdapter.java
@@ -53,17 +53,17 @@ public class ShixiaoAdapter extends RecyclerView.Adapter<ShixiaoAdapter.ViewHold
         try {
             address = "地址：" + list.get(position).getFarmlandsInfo().getFarmlandsProvince() + list.get(position).getFarmlandsInfo().getFarmlandsCity() + list.get(position).getFarmlandsInfo().getFarmlandsCounty() + list.get(position).getFarmlandsInfo().getFarmlandsVillage();
         }catch (Exception e){
-            address = "地址：[无效数据控制]";
+            address = "地址：[null]";
         }
         try {
             area = "\n面积：" + list.get(position).getFarmlandsInfo().getFarmlandsArea();
         }catch (Exception e){
-            area = "\n面积：[无效数据控制]";
+            area = "\n面积：[null]";
         }
         try {
             price = "\n单价：" + list.get(position).getFarmlandsInfo().getFarmlandsUnitPrice();
         }catch (Exception e){
-            price = "地址：[无效数据控制]";
+            price = "地址：[null]";
         }
         try {
             phone = "\n电话：" + list.get(position).getFarmlandsInfo().getPersonInfoFarmerMachine().getPersonPhone();
